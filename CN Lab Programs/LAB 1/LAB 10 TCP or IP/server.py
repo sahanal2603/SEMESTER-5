da7dd2512ee8fbd9227c0b9946dbc9076d41742c
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2020-12-17 14:03:41 +0530

    Add files via upload

---
+++ CN Lab Programs/LAB 1/LAB 10 TCP or IP/server.py
@@ -0,0 +1,17 @@
+from socket import *
+serverName='DESKTOP-L5TO248'
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,serverPort))
+serverSocket.listen(1)
+print ("The server is ready to receive")
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    
+    file=open(sentence,"r")
+    l=file.read(1024)
+    
+    connectionSocket.send(l.encode())
+    file.close()
+    connectionSocket.close()
